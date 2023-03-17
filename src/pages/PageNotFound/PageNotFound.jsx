de341165ca04f83166d8f0e16f3d15907f64f13d
Thomas <mthomas.pro@outlook.fr>
2023-03-17 13:27:59 +0100

    v0.3.0
    - page not found created
    - page style ''page not found'' completed

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './pagenotfound.scss';
 
 export function PageNotFound() {
     return (
-        <div>
-            404
-        </div>
+        <main className='page-not-found'>
+            <div>
+                <h1>404</h1>
+                <h2>Oups! La page que vous demandez n'existe pas.</h2>
+            </div>
+            <Link to='/'>Retourner sur la page d'accueil</Link>
+        </main>
     );
 }
