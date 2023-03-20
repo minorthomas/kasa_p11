22c75199560a7b736218cbd2c86e8d422c35ca30
Thomas <mthomas.pro@outlook.fr>
2023-03-20 13:37:57 +0100

    v0.6.4
    - addition of a condition that tests if the size of the gallery is more than 1, if so, we display the buttons and the index

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -4,6 +4,7 @@ import './slider.scss';
 export function Slider({ images, title }) {
     const [currentImg, setCurrentImg] = useState(null);
     const [gallery, setGallery] = useState([]);
+    const minLength = 2
 
     useEffect(() => {
         setGallery(images);
@@ -24,34 +25,45 @@ export function Slider({ images, title }) {
         <>
             {currentImg !== null && (
                 <div className='slider'>
-                    <button
-                        className='slider_btn slider_btn-prev'
-                        onClick={handlePrev}
-                    >
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            width='24'
-                            height='24'
-                            viewBox='0 0 24 24'
-                        >
-                            <path d='M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z' />
-                        </svg>
-                    </button>
-                    <img className='slider_img' src={gallery[currentImg]} alt={title} />
-                    <p className='slider_index'>{`${currentImg + 1}/${gallery.length}`}</p>
-                    <button
-                        className='slider_btn slider_btn-next'
-                        onClick={handleNext}
-                    >
-                        <svg
-                            xmlns='http://www.w3.org/2000/svg'
-                            width='24'
-                            height='24'
-                            viewBox='0 0 24 24'
-                        >
-                            <path d='M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z' />
-                        </svg>
-                    </button>
+                    {gallery.length >= minLength && (
+                        <>
+                            <button
+                                className='slider_btn slider_btn-prev'
+                                onClick={handlePrev}
+                            >
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='24'
+                                    height='24'
+                                    viewBox='0 0 24 24'
+                                >
+                                    <path d='M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z' />
+                                </svg>
+                            </button>
+                            <p className='slider_index'>{`${currentImg + 1}/${
+                                gallery.length
+                            }`}</p>
+                            <button
+                                className='slider_btn slider_btn-next'
+                                onClick={handleNext}
+                            >
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    width='24'
+                                    height='24'
+                                    viewBox='0 0 24 24'
+                                >
+                                    <path d='M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z' />
+                                </svg>
+                            </button>
+                        </>
+                    )}
+
+                    <img
+                        className='slider_img'
+                        src={gallery[currentImg]}
+                        alt={title}
+                    />
                 </div>
             )}
         </>
