8f6687ae2c4f7aa26ba9a4ec637d0a7a586d7d51
Thomas <mthomas.pro@outlook.fr>
2023-04-13 18:40:27 +0200

    Revert "v0.9.3"
    This reverts commit eb12676abde2d26e304815b15e9e851d8eb741b8.

--- src/pages/Housing/Housing.jsx
+++ src/pages/Housing/Housing.jsx
@@ -5,7 +5,6 @@ import { Tag } from '../../components/Tag/Tag';
 import { Dropdown } from '../../components/Dropdown/Dropdown';
 import './housing.scss';
 import { Rating } from '../../components/Rating';
-import { Loader } from '../../shared/Loader/Loader';
 
 export function Housing({ data, isLoading, error }) {
     const { id } = useParams();
@@ -19,6 +18,7 @@ export function Housing({ data, isLoading, error }) {
                     return housing.id === id;
                 });
                 if (filteredHousing.length === 0) {
+                    // return <Navigate to='/housing/404' replace={true}/>;
                     return navigate('/404');
                 } else {
                     setHousing(filteredHousing[0]);
@@ -33,7 +33,7 @@ export function Housing({ data, isLoading, error }) {
     }
 
     if (isLoading) {
-        return <Loader />;
+        return <p>Chargement...</p>;
     }
 
     return (
